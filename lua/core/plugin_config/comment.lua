e02ef51219c032a950bcc7aef1950027f4fe2a21
Edward Hwang <hwang.edward8@gmail.com>
2023-08-03 22:20:28 -0700

    added vsc comment keybind

---
+++ lua/core/plugin_config/comment.lua
@@ -0,0 +1,10 @@
+require('Comment').setup({
+    toggler = {
+        line = '<C-_>',
+        block = '<M-/>',
+    },
+    opleader = {
+        line = '<C-_>',
+        block = '<M-/>',
+    },
+})
