988ef97a70ebab56e527384c9f61445fff7ffec3
ehg <hwang.edward8@gmail.com>
2023-10-01 22:39:19 -0700

    Updated Indent Blankline
    breaking changes are mega cringe holy moly

--- lua/core/plugin_config/indent-blankline.lua
+++ lua/core/plugin_config/indent-blankline.lua
@@ -1,11 +1,14 @@
-require('indent_blankline').setup({
+require('ibl').setup({
     show_trailing_blankline_indent = false,
 })
 
-vim.g.indent_blankline_buftype_exclude = { 'terminal', 'nofile' }
-vim.g.indent_blankline_filetype_exclude = {
-    'help',
-    'dashboard',
-    'NvimTree',
-    'Trouble'
-}
+require('ibl').overwrite({
+    exclude = {
+        filetypes = {
+            'help',
+            'dashboard',
+            'NvimTree',
+            'Trouble'
+        }
+    }
+})
