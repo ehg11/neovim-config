4d14bd338c84dbf195f8f1fb439c503d25149ac9
ehg <hwang.edward8@gmail.com>
2023-11-04 18:26:44 -0700

    don't hide ignored files

--- lua/core/plugin_config/nvim-tree.lua
+++ lua/core/plugin_config/nvim-tree.lua
@@ -1,6 +1,22 @@
---[[ vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1 ]]
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
 
-require('nvim-tree').setup()
+require('nvim-tree').setup({
+    diagnostics = {
+        enable = true,
+        icons = {
+            error = " ",
+            warning = " ",
+            hint = " ",
+            info = " ",
+        },
+    },
+    git = {
+        enable = true,
+    },
+    filters = {
+        git_ignored = false,
+    }
+})
 
 vim.keymap.set('n', '<C-n>', ':NvimTreeFindFileToggle<CR>', { noremap = true, silent = true, desc = 'Open File Tree'})
