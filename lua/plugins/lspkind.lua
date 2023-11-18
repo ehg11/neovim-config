e8ac95ebb82e8b26aa8a49caf3bbe6924b7da2cb
ehg <hwang.edward8@gmail.com>
2023-11-17 20:52:12 -0800

    added aerial back but with icons and stuff

--- lua/plugins/lspkind.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'onsails/lspkind.nvim',
-    config = function ()
-        local lspkind = require('lspkind')
-        lspkind.init({
-            mode = 'symbol_text',
-        })
-    end
-}
