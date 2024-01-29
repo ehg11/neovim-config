4c2b1e6b67b9eda54a2fab7f1f8253491bf65c6f
ehg <hwang.edward8@gmail.com>
2024-01-29 00:07:13 -0800

    added border to doc cmp window

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -28,7 +28,10 @@ return {
                 end
             },
             window = {
-                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
+                completion = cmp.config.window.bordered({
+                    winhighlight = 'Normal:CmpPmenu,CursorLine:PmenuSel,Search:None'
+                }),
             },
             mapping = cmp.mapping.preset.insert({
                 ['<C-k>'] = cmp.mapping.select_prev_item(),
