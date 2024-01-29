30e7a44795b01bd66faa0e0971e7c5a3e53d6bf5
ehg <hwang.edward8@gmail.com>
2024-01-28 21:48:25 -0800

    commented luasnip
    * usually, i don't want to see snippets
    * brain too smol

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -45,10 +45,10 @@ return {
             }),
             sources = cmp.config.sources({
                 { name = 'nvim_lsp' },
-                { name = 'luasnip' },
                 { name = 'nvim_lsp_signature_help' },
                 { name = 'buffer' },
                 { name = 'path' },
+                -- { name = 'luasnip' },
             }),
         })
     end
