3e7edd83138c831d92d36ad5a30f6b1125e87328
ehg <hwang.edward8@gmail.com>
2024-02-01 04:52:13 -0800

    rainbow with block languages looks weird

--- lua/plugins/rainbow-delimiters.lua
+++ lua/plugins/rainbow-delimiters.lua
@@ -4,21 +4,8 @@ return {
         "nvim-treesitter/nvim-treesitter"
     },
     config = function()
-        local rainbow = require('rainbow-delimiters')
         local rainbow_setup = require('rainbow-delimiters.setup')
         rainbow_setup.setup({
-            strategy = {
-                [''] = rainbow.strategy['global'],
-                vim = rainbow.strategy['local'],
-            },
-            query = {
-                [''] = 'rainbow-delimiters',
-                lua = 'rainbow-blocks',
-            },
-            priority = {
-                [''] = 110,
-                lua = 210,
-            },
             highlight = {
                 'RainbowDelimiterRed',
                 'RainbowDelimiterYellow',
