6adc203592ff17b05052a7da8c711a7fc733a915
Edward Hwang <hwang.edward8@gmail.com>
2024-02-17 20:18:43 -0800

    added vscode options
    * only loading surround nvim

--- init.lua
+++ init.lua
@@ -2,23 +2,38 @@ require('core')
 
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  }
+    vim.fn.system {
+        'git',
+        'clone',
+        '--filter=blob:none',
+        'https://github.com/folke/lazy.nvim.git',
+        '--branch=stable', -- latest stable release
+        lazypath,
+    }
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup('plugins', {
-    checker = {
-      enabled = true,
-      notify = false,
-    },
-    change_detection = {
-      notify = false,
-    },
-})
+if vim.g.vscode then
+    require('lazy').setup({
+        {
+            'kylechui/nvim-surround',
+            version = '*',
+            event = 'VeryLazy',
+            config = function()
+                local surround = require('nvim-surround')
+                surround.setup({ })
+            end
+        },
+    })
+else
+    require('lazy').setup('plugins', {
+        checker = {
+            enabled = true,
+            notify = false,
+        },
+        change_detection = {
+            notify = false,
+        },
+    })
+end
+
