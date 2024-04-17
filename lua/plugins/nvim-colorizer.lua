3d2529995742a0764590c1e0f91bcc4484c771b8
ehg <hwang.edward8@gmail.com>
2024-04-16 23:52:58 -0700

    fixed nvim-colorizer

--- lua/plugins/nvim-colorizer.lua
+++ lua/plugins/nvim-colorizer.lua
@@ -1,14 +1,23 @@
 return {
-    'norcalli/nvim-colorizer.lua',
-    event = 'BufRead',
+    'NvChad/nvim-colorizer.lua',
     config = function()
-        require('colorizer').setup({
-            'css',
-            'js',
-            'ts',
-            'jsx',
-            'tsx',
-            'html',
+        local colorizer = require('colorizer')
+        colorizer.setup({
+            filetypes = {
+                'css',
+                'html',
+                'js',
+                'jsx',
+                'ts',
+                'tsx',
+            },
+            user_default_options = {
+                rgb_fn = true,
+                hsl_fn = true,
+                css = true,
+                css_fn = true,
+                tailwind = true,
+            }
         })
     end,
 }
