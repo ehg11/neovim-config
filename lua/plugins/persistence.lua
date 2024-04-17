77312b1926920c08d697ff7bd2ad990da1d06b88
ehg <hwang.edward8@gmail.com>
2024-04-16 23:52:44 -0700

    updated persistence to use similar opts to lazy.vim

--- lua/plugins/persistence.lua
+++ lua/plugins/persistence.lua
@@ -1,11 +1,15 @@
 return {
-    "folke/persistence.nvim",
-    event = "BufReadPre", -- this will only start session saving when an actual file was opened
+    'folke/persistence.nvim',
+    event = 'BufReadPre', -- this will only start session saving when an actual file was opened
     config = function()
         local persistence = require('persistence')
-        persistence.setup()
+        persistence.setup({
+            options = vim.opt.sessionoptions:get()
+        })
 
-        vim.keymap.set('n', '<leader>rs', function() persistence.load() end, {
+        vim.keymap.set('n', '<leader>rs', function()
+            persistence.load({ last = true })
+        end, {
             desc = '[R]estore [S]esssion',
             silent = true,
             noremap = true,
