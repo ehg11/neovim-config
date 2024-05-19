8dbe461a82ef90404fc086426cf2a952863bf4b2
ehg <hwang.edward8@gmail.com>
2024-05-18 23:33:29 -0700

    replaced conform and nvim-lint with none-ls
    something weird was going on with eslint and I couldn't be bothered to fix it

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,46 @@
+return {
+    'nvimtools/none-ls.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+        'nvimtools/none-ls-extras.nvim',
+        'nvim-lua/plenary.nvim',
+    },
+    config = function()
+        local null_ls = require('null-ls')
+        local sources = {
+            -- formatting
+            null_ls.builtins.formatting.stylua,
+            -- null_ls.builtins.formatting.clang_format,
+            null_ls.builtins.formatting.prettierd,
+            null_ls.builtins.formatting.isort,
+            null_ls.builtins.formatting.black,
+            -- linters
+            require('none-ls.diagnostics.eslint'),
+        }
+
+        null_ls.setup({
+            sources = sources,
+            on_attach = function(client, bufnr)
+                if client.supports_method('textDocument/formatting') then
+                    vim.api.nvim_create_autocmd('BufWritePre', {
+                        group = vim.api.nvim_create_augroup(
+                            'ehg_format_on_save',
+                            { clear = true }
+                        ),
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format({ async = false })
+                        end,
+                    })
+                end
+            end,
+        })
+
+        vim.keymap.set(
+            { 'n', 'v' },
+            '<leader>fm',
+            vim.lsp.buf.format,
+            { desc = '[F]or[m]at file or range (in visual mode)' }
+        )
+    end,
+}
